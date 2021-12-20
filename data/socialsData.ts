0e390e779ec591d9d4c05531923e8008a4161711
Alvin Cherk <alvin@cherk.org>
2021-12-20 13:23:30 +1100

    Add social data file

---
+++ data/socialsData.ts
@@ -0,0 +1,20 @@
+// This file contains all the social media contacts
+
+/**
+ * This is the **PUBLIC** facebook page for MTRNSoc.
+ *
+ * I.e., accessible without a facebook account
+ */
+export const facebookPage = "https://www.facebook.com/UNSWMTRNSOC";
+
+/**
+ * This is the **PRIVATE** facebook group for MTRNSoc.
+ *
+ * I.e., members need to be verified before seeing the page
+ */
+export const facebookGroup = "https://www.facebook.com/groups/unsw.mechatronics/";
+
+export const linkedin = "https://www.linkedin.com/company/unsw-mechatronics-society/";
+export const instagram = "https://www.instagram.com/unsw_mechatronics/";
+export const contactEmail = "mechatronics.unsw@gmail.com";
+export const discord = "https://discord.gg/2esUSwZzuK)";
