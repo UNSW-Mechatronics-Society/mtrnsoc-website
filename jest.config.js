b9a9bb657f91fed8a56f4a0a6bba4422d3533c84
Alvin Cherk <alvin@cherk.org>
2022-01-13 22:10:29 +1100

    Add jest setup

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^components(.*)$": "<rootDir>/components$1",
+    "^pages(.*)$": "<rootDir>/pages$1",
+  },
+};
