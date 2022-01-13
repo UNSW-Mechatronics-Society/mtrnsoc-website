10eb07624d7189ae439038928a2bb0be4406196f
Alvin Cherk <alvin@cherk.org>
2022-01-14 03:23:34 +1100

    Don't use Layout on ErrorPage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import type { AppProps } from "next/app";
 import { Layout } from "components";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  if (Component.displayName === "ErrorPage") {
+    return <Component {...pageProps} />;
+  }
   return (
     <Layout>
       <Component {...pageProps} />
