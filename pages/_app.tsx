9105c944863bfbf47d8e664b8c24ceb02bbaf27a
Alvin <39092498+Kuroson@users.noreply.github.com>
2022-01-17 16:16:47 +1100

    Basic api endpoints for events (#14)
    * Add jest setup
    
    * Remove babel config file to use SWC
    
    * Add path for data and style folders for jest
    
    * Add basic events api routes
    
    * Add event handler jest tests
    
    * Fix bug
    
    * Add new event
    
    * Update package.json and add test to CI
    
    * Don't use Layout on ErrorPage
    
    * Add swagger UI
    
    * Add swaggerUI documentation for /api/events
    
    * Layout update
    
    * Add swaggerUI documentation for /api/currentEvents

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from "next/app";
 import { Layout } from "components";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  if (Component.displayName === "ErrorPage" || Component.displayName === "NoLayout") {
+    // Currently using `displayName` to remove Layout formatting
+    return <Component {...pageProps} />;
+  }
   return (
     <Layout>
       <Component {...pageProps} />
