279161f48f9b44a8014c889e74d64e48fcb8962d
Alvin Cherk <alvin@cherk.org>
2022-01-30 01:20:39 +1100

    Tweaked spacing

--- components/Events/OurEvents.tsx
+++ components/Events/OurEvents.tsx
@@ -2,6 +2,8 @@ import eventData from "data/eventsData";
 import Link from "next/link";
 
 // TODO: Arrow functionality & movement
+// TODO: Build out mobile functionality
+// TODO: Request from backend
 
 const getEventsToDisplay = () => {
   return [...eventData].slice(0, 3);
@@ -12,7 +14,7 @@ export default function OurEvents(): JSX.Element {
 
   return (
     <div className="flex flex-col w-full">
-      <div className="flex flex-row w-full justify-between pb-6">
+      <div className="flex flex-row w-full justify-between pb-10">
         {eventsToDisplay.map((event) => (
           <img
             src={event.imagePath}
@@ -25,7 +27,7 @@ export default function OurEvents(): JSX.Element {
       <div>
         <Link href="/events">
           <a>
-            <button className="bg-onyx text-off-white px-3.5 py-3 rounded-3xl font-medium text-base">
+            <button className="bg-onyx text-off-white px-6 py-3 rounded-3xl font-normal text-lg">
               View our events here
             </button>
           </a>
