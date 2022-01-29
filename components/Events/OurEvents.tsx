f4ec9e57b9a2d4e567ba8dd40301d8ed813ba4b1
Alvin Cherk <alvin@cherk.org>
2022-01-30 01:32:57 +1100

    Add hover expansion with animation and shadow effect

--- components/Events/OurEvents.tsx
+++ components/Events/OurEvents.tsx
@@ -16,18 +16,19 @@ export default function OurEvents(): JSX.Element {
     <div className="flex flex-col w-full">
       <div className="flex flex-row w-full justify-between pb-10">
         {eventsToDisplay.map((event) => (
-          <img
-            src={event.imagePath}
-            key={event.title}
-            alt={`${event.title} event banner`}
-            className="h-60"
-          />
+          <a key={event.title} href={event.facebookEventLink} target="_blank" rel="noreferrer">
+            <img
+              src={event.imagePath}
+              alt={`${event.title} event banner`}
+              className="h-60 hover:scale-[1.05] duration-100 hover:shadow-2xl"
+            />
+          </a>
         ))}
       </div>
       <div>
         <Link href="/events">
           <a>
-            <button className="bg-onyx text-off-white px-6 py-3 rounded-3xl font-normal text-lg">
+            <button className="bg-onyx text-off-white px-6 py-3 rounded-3xl font-normal text-lg hover:scale-[1.05] duration-100 hover:shadow">
               View our events here
             </button>
           </a>
