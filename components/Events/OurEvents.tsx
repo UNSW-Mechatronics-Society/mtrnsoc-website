22966982dc9edcd828511d4114fac4de95ab442d
Alvin Cherk <alvin@cherk.org>
2022-02-04 19:41:08 +1100

    Current events section update

--- components/Events/OurEvents.tsx
+++ components/Events/OurEvents.tsx
@@ -1,65 +1,103 @@
 import React from "react";
 import type { eventDetails } from "data/eventsData";
 import Link from "next/link";
-import useWindowDimensions from "util/useWindowDimensions";
-
-// TODO: Arrow functionality & movement
-// TODO: Build out mobile functionality
+import styles from "./OurEvents.module.scss";
 
 export type OurEventsProps = {
   currentEvents: eventDetails[] | null;
 };
 
-/**
- * Returns the number of events to display in array
- * @param width width of screen
- * @param currentEvents events assumed to be in order of dates
- * @returns number of events to display
- */
-const calculateEventsToDisplay = (width: number, currentEvents: eventDetails[]): eventDetails[] => {
-  if (currentEvents.length === 0) return [];
-  if (currentEvents.length === 1 || width <= 1000) return [...currentEvents].splice(0, 1);
-  if (currentEvents.length === 2 || width <= 1450) return [...currentEvents].splice(0, 2);
-  return [...currentEvents].splice(0, 3);
+type TemplateProps = {
+  children: React.ReactNode;
 };
 
-export default function OurEvents({ currentEvents }: OurEventsProps): JSX.Element {
-  const { width } = useWindowDimensions();
-
-  const FailErrorMessage = () => {
-    return <p className="w-full grid place-items-center text-xl">Failed to load events</p>;
-  };
-
+const Template = ({ children }: TemplateProps) => {
   return (
-    <div className="flex flex-col w-full">
-      <div className="flex flex-row w-full justify-around pb-10">
-        {currentEvents ? (
-          currentEvents.length === 0 ? (
-            <div>There are no events. Please check back later!</div>
-          ) : (
-            calculateEventsToDisplay(width, currentEvents).map((event) => (
-              <a key={event.title} href={event.facebookEventLink} target="_blank" rel="noreferrer">
-                <img
-                  src={event.imagePath}
-                  alt={`${event.title} event banner`}
-                  className="h-60 hover:scale-[1.05] duration-100 hover:shadow-2xl"
-                />
-              </a>
-            ))
-          )
-        ) : (
-          <FailErrorMessage />
-        )}
-      </div>
+    <div className={styles.mainContainer}>
+      <div className={styles.eventsContainer}>{children}</div>
       <div>
         <Link href="/events">
           <a>
-            <button className="bg-onyx text-off-white px-6 py-3 rounded-3xl font-normal text-lg hover:scale-[1.05] duration-100 hover:shadow">
-              View our events here
-            </button>
+            <button className={styles.button}>View all our events</button>
           </a>
         </Link>
       </div>
     </div>
+  );
+};
+
+export default function OurEvents({ currentEvents }: OurEventsProps): JSX.Element {
+  const [currentPos, setCurrentPos] = React.useState(0); // position in array, indexed at 0
+
+  if (!currentEvents) {
+    // Failed to get current events
+    return (
+      <Template>
+        <p className={styles.errorText}>Failed to load events</p>
+      </Template>
+    );
+  }
+
+  if (currentEvents.length === 0) {
+    // No events to display
+    return (
+      <Template>
+        <p className={styles.errorText}>There are no events currently. Check back later!</p>
+      </Template>
+    );
+  }
+
+  const nextSlide = () =>
+    setCurrentPos(currentPos === currentEvents.length - 1 ? 0 : currentPos + 1);
+  const prevSlide = () =>
+    setCurrentPos(currentPos === 0 ? currentEvents.length - 1 : currentPos - 1);
+
+  const currentEventToDisplay = currentEvents[currentPos];
+  const { title, imagePath } = currentEventToDisplay;
+
+  return (
+    <Template>
+      <div className={styles.imageSliderContainer}>
+        <div className={styles.navigationContainer}>
+          <img
+            src="/misc/chevron--left.svg"
+            alt="left navigation"
+            aira-label="forward navigation"
+            className={styles.navigationButtons}
+            onClick={prevSlide}
+          />
+        </div>
+        <div className="mx-3">
+          {currentEvents.map((event, index) => (
+            <div
+              key={index}
+              className={`${index === currentPos ? styles.slideActive : styles.slide}`}
+            >
+              <Link href={event.facebookEventLink}>
+                <a target="_blank">
+                  <img
+                    src={event.imagePath}
+                    alt={`${event.title} banner`}
+                    className={`${styles.eventsBannerImage} ${
+                      index === currentPos ? "block" : "hidden"
+                    }`}
+                    title={event.title}
+                  />
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className={styles.navigationContainer}>
+          <img
+            src="/misc/chevron--right.svg"
+            alt="right navigation"
+            aira-label="backward navigation"
+            className={styles.navigationButtons}
+            onClick={nextSlide}
+          />
+        </div>
+      </div>
+    </Template>
   );
 }
