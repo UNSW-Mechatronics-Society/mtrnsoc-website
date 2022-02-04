b3cf36a2427d3786d429d71413b966bf34666345
Alvin <39092498+Kuroson@users.noreply.github.com>
2022-02-04 18:23:37 +1100

    Arrow Down on homepage banner (#33)
    * Arrow down on banner
    
    * Make arrow optional in component

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -3,9 +3,14 @@ import styles from "./Banner.module.scss";
 type BannerProps = {
   imgURL: string;
   text?: string;
+  arrow: boolean;
 };
 
-export default function Banner({ imgURL, text = undefined }: BannerProps): JSX.Element {
+export default function Banner({
+  imgURL,
+  text = undefined,
+  arrow = false,
+}: BannerProps): JSX.Element {
   return (
     <div className={styles.container}>
       <img src={imgURL} alt="hero banner" className={styles.image} draggable="false" />
@@ -13,6 +18,11 @@ export default function Banner({ imgURL, text = undefined }: BannerProps): JSX.E
       {text && (
         <div className={styles.textContainer}>
           <h1 className={styles.titleText}>{text.toUpperCase()}</h1>
+        </div>
+      )}
+      {arrow && (
+        <div className={styles.arrowDown}>
+          <img src="/arrowDown.svg" alt="" className={styles.arrowDown} draggable={false} />
         </div>
       )}
     </div>
