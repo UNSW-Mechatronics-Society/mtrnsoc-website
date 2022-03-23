2985712fd36261e029065ca66f5df2cf28d23156
Alvin Cherk <alvin@cherk.org>
2022-03-24 01:39:52 +1100

    Filter events correctly

--- pages/events.tsx
+++ pages/events.tsx
@@ -3,10 +3,11 @@ import eventData, { eventDetails } from "data/eventsData";
 import type { GetStaticProps, NextPage } from "next";
 
 type EventsPageProps = {
-  allEvents: eventDetails[];
+  currentEvents: eventDetails[];
+  pastEvents: eventDetails[];
 };
 
-const Home: NextPage<EventsPageProps> = ({ allEvents }) => {
+const Home: NextPage<EventsPageProps> = ({ currentEvents, pastEvents }) => {
   return (
     <div className="h-full">
       <MetaTags title="Events - MTRNSoc" description="Society events" />
@@ -15,9 +16,9 @@ const Home: NextPage<EventsPageProps> = ({ allEvents }) => {
       <ContentContainer>
         <div className="flex h-full my-12 flex-col items-center">
           <h1 className="text-4xl font-semibold mb-6">Current Events</h1>
-          {allEvents.map((e) => {
+          {currentEvents.map((e) => {
             return (
-              <div key={e.title} className="w-full grid place-items-center my-3">
+              <div key={e.title} className="w-full grid place-items-center my-5">
                 <EventCardHorizontal eventData={e} />
               </div>
             );
@@ -29,8 +30,17 @@ const Home: NextPage<EventsPageProps> = ({ allEvents }) => {
 };
 
 export const getStaticProps: GetStaticProps<EventsPageProps> = async () => {
-  // TODO: Currently lists all events
-  return { props: { allEvents: eventData } };
+  const currentEvents = eventData.filter((x) => {
+    const oldestDate = Math.max(
+      ...x.date.map((y) => (y.endDate !== null ? y.endDate : y.startDate)),
+    );
+    // as Date.now() is in milliseconds
+    return oldestDate * 1000 >= Date.now();
+  });
+  currentEvents.reverse();
+
+  const pastEvents = eventData.filter((x) => !currentEvents.includes(x));
+  return { props: { currentEvents, pastEvents } };
 };
 
 export default Home;
