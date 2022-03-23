5311f67ac5a180f029bdadf63d21f0d4d50525a4
Alvin Cherk <alvin@cherk.org>
2022-03-24 01:36:29 +1100

    Implementation of event card

--- components/Events/EventCardHorizontal.tsx
+++ components/Events/EventCardHorizontal.tsx
@@ -1,5 +1,6 @@
 import { eventDate, eventDetails } from "data/eventsData";
 import moment from "moment";
+import Link from "next/link";
 
 type EventCardHorizontalProps = {
   eventData: eventDetails;
@@ -29,7 +30,7 @@ const convertUnixToString = ({ startDate, endDate }: eventDate) => {
 
   if (!endDate) {
     // Event with only a starting time
-    return `${startMoment.format("D MMMM")}, ${convertToTimeString(startMoment)}`;
+    return `${startMoment.format("D MMM")}, ${convertToTimeString(startMoment)}`;
   }
 
   const endMoment = moment.unix(endDate);
@@ -40,32 +41,41 @@ const convertUnixToString = ({ startDate, endDate }: eventDate) => {
     startMoment.isSameOrBefore(endMoment, "year")
   ) {
     // Event Lies on the same day
-    return `${startMoment.format("D MMMM")}, ${convertToTimeString(
+    return `${startMoment.format("D MMM")}, ${convertToTimeString(
       startMoment,
     )} - ${convertToTimeString(endMoment)}`;
   }
   // Multi day event
-  return `${startMoment.format("D MMMM")}, ${convertToTimeString(startMoment)} - ${endMoment.format(
-    "D MMMM",
+  return `${startMoment.format("D MMM")}, ${convertToTimeString(startMoment)} - ${endMoment.format(
+    "D MMM",
   )}, ${convertToTimeString(endMoment)}`;
 };
 
 export default function EventCardHorizontal({ eventData }: EventCardHorizontalProps): JSX.Element {
   return (
-    <div className="flex flex-row bg-[#E8EFF5] w-[65%] rounded-2xl h-full">
+    <div className="flex flex-row bg-[#E8EFF5] w-[60%] rounded-2xl h-full">
       <img
         className="w-[32rem] rounded-l-2xl"
         src={eventData.imagePath}
         alt={`${eventData.title} image banner`}
       />
-      <div className="px-6 pt-8 w-full flex flex-col h-full outline">
+      <div className="px-6 pt-8 w-full flex flex-col h-full">
         <h1 className="text-2xl font-semibold uppercase w-full text-left">{eventData.title}</h1>
         <div className="mt-5 w-full">
           <div className="flex flex-row">
             <img className="mr-3" src="/misc/calendar--heat-map.svg" alt="" draggable="false" />
-            <p className="text-lg grid place-items-center">
-              {convertUnixToString(eventData.date[0])}
-            </p>
+            <div className="flex flex-col">
+              {eventData.date.map((data, index) => {
+                return (
+                  <p
+                    className="text-lg grid place-items-center"
+                    key={`${eventData.title}-${index}`}
+                  >
+                    {convertUnixToString(data)}
+                  </p>
+                );
+              })}
+            </div>
           </div>
           {/* If location exists, display it */}
           {eventData.location && (
@@ -75,10 +85,12 @@ export default function EventCardHorizontal({ eventData }: EventCardHorizontalPr
             </div>
           )}
         </div>
-        <div className="w-full h-full">
-          <button className="text-[#5FA8D3] border-[#5FA8D3] border-[1px] px-3 py-1 rounded-md">
-            <p className="text-base">Find More Info</p>
-          </button>
+        <div className="w-full h-full place-items-end grid mb-5">
+          <a href={eventData.facebookEventLink} target="_blank" rel="noreferrer">
+            <button className="text-[#5FA8D3] border-[#5FA8D3] border-[1px] px-3 py-1 rounded-md hover:bg-[#61b4d5] hover:text-culturedWhite">
+              <p className="text-base">Find More Info</p>
+            </button>
+          </a>
         </div>
       </div>
     </div>
