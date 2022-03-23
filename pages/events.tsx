79d439b926698a957784b5a95da3cd1340c7fd2f
Alvin Cherk <alvin@cherk.org>
2022-03-24 01:54:11 +1100

    Temp

--- pages/events.tsx
+++ pages/events.tsx
@@ -8,6 +8,26 @@ type EventsPageProps = {
 };
 
 const Home: NextPage<EventsPageProps> = ({ currentEvents, pastEvents }) => {
+  const CurrentEventsSection = () => {
+    if (currentEvents.length >= 1) {
+      // There exists a current event
+      return (
+        <>
+          {currentEvents.map((e) => {
+            return (
+              <div key={e.title} className="w-full grid place-items-center my-5">
+                <EventCardHorizontal eventData={e} />
+              </div>
+            );
+          })}
+        </>
+      );
+    } else {
+      // TODO: No current event display
+      return <div>No events currently :(</div>;
+    }
+  };
+
   return (
     <div className="h-full">
       <MetaTags title="Events - MTRNSoc" description="Society events" />
@@ -16,13 +36,12 @@ const Home: NextPage<EventsPageProps> = ({ currentEvents, pastEvents }) => {
       <ContentContainer>
         <div className="flex h-full my-12 flex-col items-center">
           <h1 className="text-4xl font-semibold mb-6">Current Events</h1>
-          {currentEvents.map((e) => {
-            return (
-              <div key={e.title} className="w-full grid place-items-center my-5">
-                <EventCardHorizontal eventData={e} />
-              </div>
-            );
-          })}
+          <CurrentEventsSection />
+        </div>
+      </ContentContainer>
+      <ContentContainer customBackgroundColour="bg-uranian-blue">
+        <div className="flex h-full my-12 flex-col items-center">
+          <h1 className="text-4xl font-semibold mb-6">Past Events</h1>
         </div>
       </ContentContainer>
     </div>
@@ -40,7 +59,12 @@ export const getStaticProps: GetStaticProps<EventsPageProps> = async () => {
   currentEvents.reverse();
 
   const pastEvents = eventData.filter((x) => !currentEvents.includes(x));
-  return { props: { currentEvents, pastEvents } };
+
+  return {
+    props: { currentEvents, pastEvents },
+    // Rebuild page every 5 minutes
+    revalidate: 5 * 60,
+  };
 };
 
 export default Home;
