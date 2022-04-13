cc7435047e2be93de3cf7cd2ec16b37044a2aa5d
Alvin Cherk <alvin@cherk.org>
2022-04-13 18:04:06 +1000

    Remove unncessary keys

--- components/Profile/ProfileCards.tsx
+++ components/Profile/ProfileCards.tsx
@@ -13,44 +13,26 @@ const ProfileCards = ({ profileData, background, contactEmail }: ProfileCardsPro
   const backgroundClass = background === "director" ? styles.directorBGColour : styles.execBGColour;
   return (
     <div className={styles.mainContainer}>
-      {profileData.map((profile, indx) => {
+      {profileData.map((profile) => {
         return (
-          <>
-            <div className={`${styles.profileContainer} ${backgroundClass}`}>
-              <img
-                key={`img-${indx}`}
-                src={profile.src}
-                alt={profile.id}
-                className={styles.picture}
-              />
-              <div className={styles.textContainer}>
-                <h1 key={`h1-${indx}`} className={styles.name}>
-                  {profile.id}
-                </h1>
-                <div key={`h1-${indx}`} className={styles.position}>
-                  {profile.position}
-                </div>
-              </div>
-              <div className={styles.links}>
-                <a rel="noreferrer" target="_blank" href={profile.linkedIn}>
-                  <img
-                    key={`img-${indx}`}
-                    src="/logos/linkedinLogo.svg"
-                    alt="LinkedInLogo"
-                    className={styles.logo}
-                  />
-                </a>
-                <a href={`mailto:${contactEmail}`}>
-                  <img
-                    key={`img-${indx}`}
-                    src={"/logos/emailLogo.svg"}
-                    alt={"LinkedInLogo"}
-                    className={styles.logo}
-                  />
-                </a>
-              </div>
+          <div
+            className={`${styles.profileContainer} ${backgroundClass}`}
+            key={`${profile.position} card`}
+          >
+            <img src={profile.src} alt={profile.id} className={styles.picture} />
+            <div className={styles.textContainer}>
+              <h1 className={styles.name}>{profile.id}</h1>
+              <div className={styles.position}>{profile.position}</div>
             </div>
-          </>
+            <div className={styles.links}>
+              <a rel="noreferrer" target="_blank" href={profile.linkedIn}>
+                <img src="/logos/linkedinLogo.svg" alt="LinkedInLogo" className={styles.logo} />
+              </a>
+              <a href={`mailto:${contactEmail}`}>
+                <img src="/logos/emailLogo.svg" alt="LinkedInLogo" className={styles.logo} />
+              </a>
+            </div>
+          </div>
         );
       })}
     </div>
--- components/Profile/SubcomProfileCards.tsx
+++ components/Profile/SubcomProfileCards.tsx
@@ -11,18 +11,17 @@ const SubcomProfileCards = ({ subcomData }: SubcomProfileCardsType): JSX.Element
     <div className={styles.subcomMainContainer}>
       {subcomData.map((team, indx) => {
         return (
-          <>
-            <div className={styles.subcomContainer}>
-              <h1 key={`h1-${indx}`} className={styles.subcomPosition}>
-                {team.portfolioName}
-              </h1>
-              <div key={`h1-${indx}`} className={styles.subcomName}>
-                {team.members.map((memberName) => (
-                  <p key={memberName}>{memberName}</p>
-                ))}
-              </div>
+          <div
+            className={styles.subcomContainer}
+            key={`${team.portfolioName} subcommittee section`}
+          >
+            <h1 className={styles.subcomPosition}>{team.portfolioName}</h1>
+            <div className={styles.subcomName}>
+              {team.members.map((memberName) => (
+                <p key={memberName}>{memberName}</p>
+              ))}
             </div>
-          </>
+          </div>
         );
       })}
     </div>
