2d25b2255134a61c5b874ebeabcfec5d0a72d900
Alvin <39092498+Kuroson@users.noreply.github.com>
2022-05-05 01:50:31 +1000

    Add sponsors page (#140)
    * Add sponsors page
    
    * Update k&a logo

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -1,16 +1,69 @@
-import type { NextPage } from "next";
-import { ContentContainer, MetaTags } from "components";
+import type { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
+import { Banner, ContentContainer, MetaTags } from "components";
+import { emailData, socialData } from "data/socialsData";
+import sponsorsData, { sponsorData } from "data/sponsorsData";
+import styles from "styles/sponsors.module.scss";
 
-const Home: NextPage = () => {
+type SponsorsPageProps = {
+  affiliates: sponsorData[];
+  sponsors: sponsorData[];
+  emailData: socialData;
+};
+
+const Sponsors: NextPage<SponsorsPageProps> = ({ affiliates, sponsors, emailData }) => {
   return (
-    <div className="flex justify-center h-full">
+    <div className="h-full">
       <MetaTags title="Sponsors - MTRNSoc" description="Society sponsors" />
-
+      <Banner
+        imgURL="/images/other/frontPageBannerEdited.png"
+        text="Sponsors"
+        arrow={true}
+        position="center"
+      />
       <ContentContainer>
-        <div className="flex justify-center h-full py-4">Sponsors Page</div>
+        <div className={styles.mainContainer}>
+          <p className={styles.textContainer}>
+            If you are interested in sponsoring our society, or would like to know more about our
+            sponsor packages, please contact us at{" "}
+            <span className="underline">
+              <Link href={emailData.url}>{emailData.display}</Link>
+            </span>
+            , or go through our contact page!
+          </p>
+          <div className={styles.sectionContainer}>
+            <h1 className={styles.sectionTitle}>Proudly Supported By</h1>
+            <div className={styles.sectionImagesContainer}>
+              {sponsors.map((x) => (
+                <img key={x.alt} src={x.src} alt={x.alt} className={styles.sectionImage} />
+              ))}
+            </div>
+          </div>
+          <div className={`${styles.sectionContainer} pt-10 pb-12`}>
+            <h1 className={styles.sectionTitle}>Affiliations</h1>
+            <div className={styles.sectionImagesContainer}>
+              {affiliates.map((x) => (
+                <img key={x.alt} src={x.src} alt={x.alt} className={styles.sectionImage} />
+              ))}
+            </div>
+          </div>
+        </div>
       </ContentContainer>
     </div>
   );
 };
 
-export default Home;
+export const getStaticProps: GetStaticProps<SponsorsPageProps> = async () => {
+  const affiliates = sponsorsData.filter((x) => x.type === "affiliate");
+  const sponsors = sponsorsData.filter((x) => x.type === "sponsor");
+
+  return {
+    props: {
+      affiliates: affiliates,
+      sponsors: sponsors,
+      emailData: emailData,
+    },
+  };
+};
+
+export default Sponsors;
