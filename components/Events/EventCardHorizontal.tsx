c0cd049059004eb5270807d4915a8a7cdb2246a2
Alvin Cherk <alvin@cherk.org>
2022-05-06 01:28:31 +1000

    Move location text to the LHS when in mobile

--- components/Events/EventCardHorizontal.tsx
+++ components/Events/EventCardHorizontal.tsx
@@ -34,7 +34,9 @@ export default function EventCardHorizontal({
           {eventData.location && (
             <div className="flex flex-row mt-3">
               <img className="mr-3" src="/misc/location.svg" alt="" draggable="false" />
-              <p className="text-lg grid place-items-center">{eventData.location}</p>
+              <p className="text-lg grid place-items-center max-eventCard:text-left">
+                {eventData.location}
+              </p>
             </div>
           )}
         </div>
