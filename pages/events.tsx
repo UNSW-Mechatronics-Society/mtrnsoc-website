e3a65ef48aaf5f1b7e9afeaa0b96b8e1bc45b24b
Alvin Cherk <alvin@cherk.org>
2022-05-08 15:53:30 +1000

    Fix title on events page

--- pages/events.tsx
+++ pages/events.tsx
@@ -138,13 +138,13 @@ const Home: NextPage<EventsPageProps> = ({ currentEventsRaw, eventsByYearByTermR
       />
       <ContentContainer customBackgroundColour="bg-uranian-blue">
         <div className={styles.mainContainer}>
-          <h1 className="text-4xl font-semibold mb-6">Current Events</h1>
+          <h1 className="text-4xl font-medium mb-6 uppercase">Current Events</h1>
           <CurrentEventsSection />
         </div>
       </ContentContainer>
       <ContentContainer>
         <div className={styles.mainContainer}>
-          <h1 className="text-4xl font-semibold mb-6">Past Events</h1>
+          <h1 className="text-4xl font-medium mb-6 uppercase">Past Events</h1>
           <div className={styles.pastEventsContainer}>
             <div className="flex justify-end w-full max-pastEventsTitle:justify-center max-pastEventsTitle:pb-3">
               <DropdownYear
