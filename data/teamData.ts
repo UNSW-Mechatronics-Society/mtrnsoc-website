6a0f71c94fc0d1819ee58e87786982f92d4d7d05
Juliet Tan <juliet.tan0601@gmail.com>
2022-06-06 00:25:18 +1000

    updated team pfp names

--- data/teamData.ts
+++ data/teamData.ts
@@ -13,31 +13,31 @@ export type SubcomProfileData = {
 // alt and name is id, email is mtrnsoc email, classnames are the same,
 export const execData: ProfileData[] = [
   {
-    src: "/images/profiles/kyra.jpg",
+    src: "/images/profiles/KyraA.jpg",
     id: "Kyra Alday",
     position: "President",
     linkedIn: "https://www.linkedin.com/in/kyra-alday/",
   },
   {
-    src: "/images/profiles/tanvee.jpg",
+    src: "/images/profiles/TanveeI.jpg",
     id: "Tanvee Islam",
     position: "Vice President",
     linkedIn: "https://www.linkedin.com/in/tanvee-mtrn/",
   },
   {
-    src: "/images/profiles/iniyan.jpg",
+    src: "/images/profiles/IniyanV.jpg",
     id: "Iniyan Vigneswaran",
     position: "Secretary",
     linkedIn: "https://www.linkedin.com/in/iniyan-vigneswaran-a5a594153/",
   },
   {
-    src: "/images/profiles/aayush.jpg",
+    src: "/images/profiles/AayushR.jpg",
     id: "Aayush Rajouria",
     position: "Treasurer",
     linkedIn: "https://au.linkedin.com/in/aayush-rajouria/",
   },
   {
-    src: "/images/profiles/brandon.jpg",
+    src: "/images/profiles/BrandonW.jpg",
     id: "Brandon Winsley",
     position: "Arc Delegate and Grievance Officer",
     linkedIn: "https://www.linkedin.com/in/bwinsley/",
@@ -46,43 +46,43 @@ export const execData: ProfileData[] = [
 
 export const directorData: ProfileData[] = [
   {
-    src: "/images/profiles/celine.jpg",
+    src: "/images/profiles/CelineC.jpg",
     id: "Celine Choo",
     position: "Marketing Director",
     linkedIn: "https://www.linkedin.com/in/celinechooliling/",
   },
   {
-    src: "/images/profiles/ishita.jpg",
+    src: "/images/profiles/IshitaK.jpg",
     id: "Ishita Katyal",
     position: "Socials Director",
     linkedIn: "www.linkedin.com/in/ishita-katyal-a75226154/",
   },
   {
-    src: "/images/profiles/janice.jpg",
+    src: "/images/profiles/JaniceN.jpg",
     id: "Janice Nyoto",
     position: "Workshops Director",
     linkedIn: "https://www.linkedin.com/in/janice-nyoto/",
   },
   {
-    src: "/images/profiles/alvin.jpg",
+    src: "/images/profiles/AlvinC.jpg",
     id: "Alvin Cherk",
     position: "IT Co-Director",
     linkedIn: "https://www.linkedin.com/in/alvin-cherk-2363291b4/",
   },
   {
-    src: "/images/profiles/juliet.jpg",
+    src: "/images/profiles/JulietT.jpg",
     id: "Juliet Tan",
     position: "IT Co-Director",
     linkedIn: "https://www.linkedin.com/in/jtannn/",
   },
   {
-    src: "/images/profiles/leonard.jpg",
+    src: "/images/profiles/LeonardC.jpg",
     id: "Leonard Chiang",
     position: "Projects Director",
     linkedIn: "https://www.linkedin.com/in/leonard-chiang/",
   },
   {
-    src: "/images/profiles/shamik.jpg",
+    src: "/images/profiles/ShamikP.jpg",
     id: "Shamik Patel",
     position: "Industry Director",
     linkedIn: "https://www.linkedin.com/in/shamik-patel/",
