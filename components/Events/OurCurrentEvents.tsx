754bc8bf74910d8c02dd2495568261ad268b9344
Juliet Tan <juliet.tan0601@gmail.com>
2022-09-05 18:19:21 +1000

    added sleep robot for no events

--- components/Events/OurCurrentEvents.tsx
+++ components/Events/OurCurrentEvents.tsx
@@ -45,11 +45,13 @@ export default function OurCurrentEvents({
   }
 
   if (currentEvents.length === 0) {
-    // TODO: No events to display
     return (
-      <Template buttonStyle={buttonStyle}>
-        <p className={styles.errorText}>There are no events currently. Check back later!</p>
-      </Template>
+      <div>
+        <img src="/zzz.svg" alt="MTRNSoc Robot Sleeping" className={styles.imageSleepContainer} />
+        <Template buttonStyle={buttonStyle}>
+          <p className={styles.errorText}>There are no events currently. Check back later!</p>
+        </Template>
+      </div>
     );
   }
 
